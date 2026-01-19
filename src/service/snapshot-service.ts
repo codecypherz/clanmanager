82a507d07cdd4903e33703dc59cc37d5b95ee883
codecypherz <james.deyerle@gmail.com>
2026-01-19 12:52:02 -0500

    Require a minimum of 1 hour between snapshots.

--- src/service/snapshot-service.ts
+++ src/service/snapshot-service.ts
@@ -4,7 +4,9 @@ import { ClanMember, ClanSnapshot } from '../model/clan-member';
 
 @Injectable({ providedIn: 'root' })
 export class SnapshotService {
-  private readonly MAX_SNAPSHOTS = 100;
+
+  private readonly MAX_SNAPSHOTS = 100; // Minimum of 4 days of history at 1 hour MIN_TIME
+  private readonly MIN_TIME_BETWEEN_SNAPSHOTS_MS = 1000 * 60 * 60; // 1 hour
 
   /**
    * Gets the full snapshot history for the clan.
@@ -24,8 +26,11 @@ export class SnapshotService {
    */
   getLatestSnapshot(clanTag: string): Observable<ClanSnapshot | null> {
     // We pipe the history and return the first element (or null if empty)
-    return this.getSnapshotHistory(clanTag).pipe(
-      map(history => (history.length > 0 ? history[0] : null)));
+    return this.getSnapshotHistory(clanTag).pipe(map(this.getLatestSnapshot_));
+  }
+
+  private getLatestSnapshot_(history: ClanSnapshot[]): ClanSnapshot|null {
+    return history.length > 0 ? history[0] : null;
   }
 
   /**
@@ -37,6 +42,12 @@ export class SnapshotService {
   saveSnapshot(clanTag: string, members: ClanMember[]): Observable<void> {
     return this.getSnapshotHistory(clanTag).pipe(
       map(history => {
+
+        if (!this.shouldSaveSnapshot(history)) {
+          console.log("Chose not to save the snapshot.");
+          return void 0;
+        }
+
         const newSnapshot: ClanSnapshot = {
           clanTag,
           timestamp: new Date(),
@@ -45,6 +56,12 @@ export class SnapshotService {
 
         // Add new snapshot to the front
         history.unshift(newSnapshot);
+
+        // Log what's happening.
+        if (history.length == this.MAX_SNAPSHOTS) {
+          console.log("Dropping the oldest snapshot");
+        }
+        console.log("Saving new snapshot: ", newSnapshot);
 
         // Keep only the last 100
         const cappedHistory = history.slice(0, this.MAX_SNAPSHOTS);
@@ -55,6 +72,16 @@ export class SnapshotService {
         return void 0; // Emit and complete.
       })
     );
+  }
+
+  private shouldSaveSnapshot(history: ClanSnapshot[]): boolean {
+    const lastSnapshot = this.getLatestSnapshot_(history);
+    if (lastSnapshot) {
+      const now = Date.now();
+      const timeSinceLastSnapshot = now - new Date(lastSnapshot.timestamp).getTime();
+      return timeSinceLastSnapshot > this.MIN_TIME_BETWEEN_SNAPSHOTS_MS;
+    }
+    return true;
   }
 
   /**
